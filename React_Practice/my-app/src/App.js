cbdaf9687a553ced58544722fb42d49fbcb151fe
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-01-17 10:58:55 +0530

    working with state

--- React_Practice/my-app/src/App.js
+++ React_Practice/my-app/src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BookList from "./components/BookList";
 import NewBook from "./components/NewBook";
 
 export default function App() {
-  const bookList = [
+  const [bookList, setBookList] = useState([
     { id: 1, name: "Atomic Habits" },
     { id: 2, name: "Zero to Hero" },
     { id: 1, name: "Word Power Made Easy" },
     { id: 1, name: "Rich Dad Poor Dad" },
-  ];
+  ]);
 
   const newBookAdded = (newBook) => {
-    bookList.push(newBook);
-    console.log("---list is--", bookList);
+    setBookList((prevBook) => {
+      return prevBook.concat(newBook);
+    });
   };
   return (
     <div>
