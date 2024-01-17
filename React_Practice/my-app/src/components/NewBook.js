48e9447f718dd564319869b5e276cd55e7003d20
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-01-17 11:57:48 +0530

    Fetching User input

--- React_Practice/my-app/src/components/NewBook.js
+++ React_Practice/my-app/src/components/NewBook.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewBook.css";
 export default function NewBook(props) {
+  const [enteredText, setEnteredText] = useState("");
+
   const handleAddBookButton = (event) => {
     event.preventDefault();
 
-    const newBook = { id: "4", name: "new book added" };
+    const newBook = { id: Math.random().toString(), name: enteredText };
     console.log("----sjbsb----", newBook);
     props.newBookAdded(newBook);
+    setEnteredText("");
+  };
+
+  const addUserBook = (event) => {
+    const inputText = event.target.value;
+    setEnteredText(inputText);
+    console.log("--input from user---", inputText);
   };
   return (
     <form className="new-book" onSubmit={handleAddBookButton}>
-      <input type="text"></input>
-      <button>ADD BOOK</button>
+      <input type="text" value={enteredText} onChange={addUserBook}></input>
+      <button type="submit">ADD BOOK</button>
     </form>
   );
 }
