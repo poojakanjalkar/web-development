0b51d7518d8ed6690b2f134f28d05e72e6857cd6
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-01-21 11:31:46 +0530

    implemented button to add new list

--- Backend/my-app/src/App.js
+++ Backend/my-app/src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ToDoList from "./components/ToDoList";
 import UserList from "./components/UserList";
 
 export default function App() {
-  const toDoList = [
+  const [toDoList, setToDoList] = useState([
     { id: "1", name: "Reading" },
     { id: "2", name: "Learning" },
     { id: "3", name: "Shopping" },
     { id: "4", name: "Playing" },
-  ];
+  ]);
+
+  const newListHandler = (newList) => {
+    setToDoList((prevList) => {
+      return prevList.concat(newList);
+    });
+  };
   return (
     <div>
       <h1 className="header">TO DO let's start</h1>
-      <UserList />
+      <UserList newListHandler={newListHandler} />
       <ToDoList toDoList={toDoList} />
     </div>
   );
--- Backend/my-app/src/components/UserList.js
+++ Backend/my-app/src/components/UserList.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./UserList.css";
 
-export default function UserList() {
+export default function UserList(props) {
+  const handleAddButton = (event) => {
+    event.preventDefault();
+    const newList = [{ id: "3", name: "my new List" }];
+    console.log(newList);
+    props.newListHandler(newList);
+  };
   return (
-    <form className="new-list">
+    <form className="new-list" onSubmit={handleAddButton}>
       <input type="text"></input>
       <button type="submit">Add To Do</button>
     </form>
