dd6de47ef79265afacbf2ee77b57a5f9d435ac70
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-01-22 11:19:08 +0530

    added state for user input

--- Backend/my-app/src/components/UserList.js
+++ Backend/my-app/src/components/UserList.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserList.css";
 
 export default function UserList(props) {
+  const [inputText, setInputText] = useState("");
+
+  const handleUserInput = (event) => {
+    setInputText(event.target.value);
+  };
   const handleAddButton = (event) => {
     event.preventDefault();
-    const newList = [{ id: "3", name: "my new List" }];
+    const newList = [{ id: Math.random().toString(), name: inputText }];
     console.log(newList);
     props.newListHandler(newList);
+    setInputText("");
   };
   return (
     <form className="new-list" onSubmit={handleAddButton}>
-      <input type="text"></input>
+      <input type="text" value={inputText} onChange={handleUserInput}></input>
       <button type="submit">Add To Do</button>
     </form>
   );
